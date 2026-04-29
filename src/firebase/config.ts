8edf3d9989289c7ac0232b49471b1edda1f74c40
Albert John <aljohme@gmail.com>
2026-04-29 11:18:32 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-6721465583-1037b",
+  "appId": "1:792224166648:web:30a0fa7660d8d5983e410d",
+  "apiKey": "AIzaSyBlun16rPe8EFCY0BbT0LdgCEam0s5nixA",
+  "authDomain": "studio-6721465583-1037b.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "792224166648"
+};
